3aac61e4e889b785720820f7f30d6d6f11d6f72a
Joan Medeiros <joanmedeiros2018@gmail.com>
2022-02-28 20:16:26 -0300

    aula sobre o uso de fragments

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React, {useState, useEffect} from 'react'
-import { ThemeContext, themes } from './Theme'
-import Card from './Card'
+import React, {Fragment} from 'react'
 
+const store = ['', '', '']
+function Column(){
+  return (
+    <tr>
+      <td>Tênis</td>
+      <td>Roupa</td>
+    </tr>
+  )
+}
+// OS fragments servem paara cabar com o problema
+// do jsx retornar apenas um elemento, ou seja, uma div
+// com vários elementos dentro
 function App () {
 
-  const [token, setToken] = useState()
-
-  useEffect(() => {
-    setTimeout(() => {
-      setToken('14615151845215')
-    }, 4000)
-  }, [setToken])
+  const renderColumns = (element, key) => {
+    return (
+      <Fragment key={key}>
+       <Column />
+    </Fragment>
+    )
+    
+   
+  }
   return(
-
-    <ThemeContext.Provider value={{...themes.primary, token}}>
-      <Card />
-    </ThemeContext.Provider>
-
-  
+    <table>
+      <tr>
+        {store.map(renderColumns)}
+      </tr>
+    </table> 
   )
 }
 
